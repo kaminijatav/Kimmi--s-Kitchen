9a9a6e4506abc5a7ed3ee45d2ac14940ba2b2dbd
Kamini <jatavkamini125@gmail.com>
2025-03-21 22:33:30 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food</h2>
+<p>
+    Choose from a diverse menu  featuring  a delectable array of dishes crafed with  the the finest ingredients
+<button>View Menu</button>
+</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
