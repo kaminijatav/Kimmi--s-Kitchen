9d5b1c573235bb021a70587eca4ae0b79de4c835
Kamini <jatavkamini125@gmail.com>
2025-03-23 19:37:47 +0530

    Created Login Sign Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,17 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
 const App = () => {
+  const[showLogin,setShowLogin]=useState(false);
+
   return (
     <>
+    {showLogin ?<LoginPopup setShowLogin={setShowLogin} />:<></>}
     <div className='app'>
-<Navbar/>
+<Navbar setShowLogin={setShowLogin}/>
 <Routes>
   <Route path='/' element={<Home/>} />
   <Route path='/cart' element={<Cart/>} />
