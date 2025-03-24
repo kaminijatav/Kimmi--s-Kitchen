83e0137d3313a373e750637fd63f408a3ee510c1
Kamini <jatavkamini125@gmail.com>
2025-03-24 10:54:23 +0530

    Update Place Order

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './Cart.css'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 const Cart = () => {
   const {cartItems,food_list,removeFromCart,getTotalCartAmount}= useContext(StoreContext);
-
+ const navigate =useNavigate();
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -58,7 +59,7 @@ const Cart = () => {
               <b>{getTotalCartAmount()+2}</b>
             </div>
           </div>
-            <button>PROCEED TO CHECKOUT</button>
+            <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
         <div>
