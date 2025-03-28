de3097f517c5353f635cf31b99d9bec8bd0ea0fa
Kamini <jatavkamini125@gmail.com>
2025-03-28 23:36:20 +0530

    Add remove API integrated to Database

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,17 +8,24 @@ const StoreContextProvider=(props)=>{
     console.log("Fetching food list from:", `${url}/api/food/list`);
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([])
-    const addToCart=(itemId)=>{
+    const addToCart=async(itemId)=>{
         if(!cartItems[itemId]){
             setCartItems(prev=>({...prev,[itemId]:1}))
         }
         else{
             setCartItems(prev=>({...prev,[itemId]:prev[itemId]+1}))
         }
+        if(token){
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
-    const removeFromCart=(itemId)=>{
+    const removeFromCart=async(itemId)=>{
        
             setCartItems(prev=>({...prev,[itemId]:prev[itemId]-1}))
+            if(token){
+                await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+
+            }
         }
 const getTotalCartAmount=()=>{
     let totalAmount=0;
@@ -35,6 +42,7 @@ const fetchFoodList=async()=>{
     const response=await axios.get(url+"/api/food/list");
     setFoodList(response.data.data)
 }
+
 useEffect(()=>{
    
     async function loadData(){
